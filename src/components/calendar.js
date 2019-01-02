23aca08f382046701778b6a45bcaa868cfe4382c
vdiaz <vdiaz@elektra.com.mx>
2019-01-02 00:49:40 -0600

    date as value and selection

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,10 +1,10 @@
 import React from "react";
 import cx from "classnames";
-//import s from "./datepicker.scss";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { addDays, addMonths, addYears, getDate } from "./util";
 
-export default class DatePicker extends React.Component {
+export default class Calendar extends React.Component {
   constructor(props) {
     super(props);
     this.today = new Date();
@@ -16,39 +16,24 @@ export default class DatePicker extends React.Component {
       en: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
       es: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"]
     };
-    this.days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    var lang = props.lang || navigator.language || navigator.userLanguage || "en";
-
-    this.state = {
-      year: props.defaultYear || this.today.getFullYear(),
-      month: props.defaultMonth || this.today.getMonth() + 1,
-      day: props.defaultDay || this.today.getDate(),
-      index: 0
-    };
-    this.state.selectionYear = this.state.year;
-    this.state.selectionMonth = this.state.month;
-    this.state.selectionDay = this.state.day;
-    if (props.defaultValue) {
-      this.state.year = Number(props.defaultValue.substring(0, 4));
-      this.state.month = Number(props.defaultValue.substring(5, 7));
-      this.state.day = Number(props.defaultValue.substring(8, 10));
-    }
-    if (props.onChange) this.onChange = props.onChange;
-    this.oldValue = this.value;
+    this.days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    this.state = { value: getDate(props.defaultValue, props.format), index: 0 };
+    this.state.selection = this.state.value;
   }
   render() {
+    const { value, selection } = this.state;
     const months = this.monthslang[this.props.lang];
     const daysofweek = this.daysofweeklang[this.props.lang];
     var style = { width: this.props.width };
     Object.assign(style, this.props.style);
     return (
-      <div className={"sitcontrol_calendar " + (this.state.index === 0 ? "pickday" : "pickmonth")}>
+      <div tabIndex="0" className={"sitcontrol_calendar " + (this.state.index === 0 ? "pickday" : "pickmonth")} onKeyUp={this.keyHandler}>
         {this.state.index === 0 ? (
           <div className="sitcontrol_header">
             <div className="sitcontrol_title">
-              <FontAwesomeIcon className="angle left" icon="angle-left" onClick={e => this.plusMonth(-1)} />
-              <span onClick={this.showMonths}>{months[this.state.selectionMonth - 1] + " " + this.state.selectionYear}</span>
-              <FontAwesomeIcon className="angle right" icon="angle-right" onClick={e => this.plusMonth(1)} />
+              <FontAwesomeIcon className="angle left" icon="angle-left" onClick={e => this.setState({ selection: addMonths(selection, -1) })} />
+              <span onClick={this.showMonths}>{months[selection.getMonth()] + " " + selection.getFullYear()}</span>
+              <FontAwesomeIcon className="angle right" icon="angle-right" onClick={e => this.setState({ selection: addMonths(selection, 1) })} />
             </div>
             <div className="sitcontrol_week">
               {daysofweek.map((o, i) => (
@@ -60,9 +45,9 @@ export default class DatePicker extends React.Component {
           </div>
         ) : (
           <div>
-            <FontAwesomeIcon className="angle left" icon="angle-left" onClick={e => this.plusYear(-1)} />
+            <FontAwesomeIcon className="angle left" icon="angle-left" onClick={e => this.setState({ selection: addYears(selection, -1) })} />
             {this.state.selectionYear}
-            <FontAwesomeIcon className="angle right" icon="angle-right" onClick={e => this.plusYear(1)} />
+            <FontAwesomeIcon className="angle right" icon="angle-right" onClick={e => this.setState({ selection: addYears(selection, 1) })} />
           </div>
         )}
         {this.state.index === 0 ? (
@@ -82,40 +67,21 @@ export default class DatePicker extends React.Component {
       </div>
     );
   }
-  changeHandler = e => this.setState({ value: this.value });
   keyHandler = e => {
     const key = e.keyCode;
     e.preventDefault();
-    if (key === 37 && this.selectionMode > 1) this.selectionMode--;
-    else if (key === 39 && this.selectionMode < 3) this.selectionMode++;
-    else if (key === 38 && this.selectionMode === 1) this.year++;
-    else if (key === 38 && this.selectionMode === 2) this.month++;
-    else if (key === 38 && this.selectionMode === 3) this.day++;
-    else if (key === 40 && this.selectionMode === 1) this.year--;
-    else if (key === 40 && this.selectionMode === 2) this.month--;
-    else if (key === 40 && this.selectionMode === 3) this.day--;
-    else if (key >= 96 && key <= 105) {
-      if (key >= 96 && key <= 98 && this.month === 1 && this.selectionMode === 2) this.month = key - 86;
-      else if (key >= 96 && key <= 97 && this.selectionMode === 2) this.month = 1;
-      else if (key > 97 && this.selectionMode === 2) this.month = key - 96;
-      else if (key >= 96 && this.day === 1 && this.selectionMode === 3) this.day = key - 86;
-      else if (key >= 96 && this.day === 2 && this.selectionMode === 3) this.day = key - 76;
-      else if (key >= 96 && this.day === 3 && this.selectionMode === 3) this.day = Math.min(key - 66, this.days[this.month]);
-      else if (key >= 97 && this.selectionMode === 3) this.day = key - 96;
-    }
-    setTimeout(this.selectText, 100);
-  };
-  selectText = () => {
-    if (this.selectionMode === 1) {
-      this.input.selectionStart = 0;
-      this.input.selectionEnd = 4;
-    } else if (this.selectionMode === 2) {
-      this.input.selectionStart = 5;
-      this.input.selectionEnd = 7;
-    } else if (this.selectionMode === 3) {
-      this.input.selectionStart = 8;
-      this.input.selectionEnd = 10;
-    }
+    if (!(key >= 33 && key <= 40)) return;
+    this.setState(newstate => {
+      if (key === 33) newstate.selection = addMonths(newstate.selection, -1);
+      else if (key === 34) newstate.selection = addMonths(newstate.selection, 1);
+      else if (key === 35) newstate.selection = addYears(newstate.selection, 1);
+      else if (key === 36) newstate.selection = addYears(newstate.selection, -1);
+      else if (key === 37) newstate.selection = addDays(newstate.selection, -1);
+      else if (key === 38) newstate.selection = addDays(newstate.selection, -7);
+      else if (key === 39) newstate.selection = addDays(newstate.selection, 1);
+      else if (key === 40) newstate.selection = addDays(newstate.selection, 7);
+      return newstate;
+    });
   };
   calculateDayOfWeek(y, m, d) {
     const t = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4];
@@ -125,81 +91,19 @@ export default class DatePicker extends React.Component {
   showMonths = e => {
     this.setState({ index: 1 });
   };
-  plusYear(n) {
-    this.setState({ selectionYear: this.state.selectionYear + n });
-  }
-  plusMonth(n) {
-    var month = this.state.selectionMonth + n;
-    var year = this.state.selectionYear;
-    if (month <= 0) {
-      month = 12;
-      year--;
-    }
-    if (month >= 13) {
-      month = 1;
-      year++;
-    }
-    this.setState({ selectionYear: year, selectionMonth: month });
-  }
-  plusDay(n) {
-    var day = this.state.day + n;
-    var month = this.state.month;
-    var year = this.state.year;
-    if (day <= 0) {
-      month--;
-      if (month === 0) {
-        month = 12;
-        year--;
-      }
-      day = this.days[month];
-    }
-    if (day > this.days[month]) {
-      day = 1;
-      month++;
-      if (month > 12) {
-        month = 1;
-        year++;
-      }
-    }
-    this.setState({ year, month, day, selectionYear: year, selectionMonth: month });
-  }
-  renderDaysList = () => {
-    const daysofweek = this.daysofweeklang[this.props.lang];
-    const firstday = this.calculateDayOfWeek(this.state.selectionYear, this.state.selectionMonth, 1);
-    var list = [];
-    for (let i = 0; i < daysofweek.length; i++) {
-      list.push(
-        <li className={"header"} key={i}>
-          {daysofweek[i]}
-        </li>
-      );
-    }
-    for (let i = 1; i <= firstday; i++) {
-      list.push(<li key={7 + i} />);
-    }
-    for (let i = 1; i <= this.days[this.state.selectionMonth]; i++) {
-      let selected = this.state.year === this.state.selectionYear && this.state.month === this.state.selectionMonth && this.state.day === i;
-      let today = this.state.selectionYear === this.today.getFullYear() && this.state.selectionMonth === this.today.getMonth() + 1 && this.today.getDate() === i;
-      list.push(
-        <li className={cx(selected && "selected", today && "today")} key={14 + i} data-value={i}>
-          {i}
-        </li>
-      );
-    }
-    return list;
-  };
   renderWeeks = () => {
     const weeks = [];
-    const startofweek = this.calculateDayOfWeek(this.state.selectionYear, this.state.selectionMonth, 1);
-    const days_month = this.state.selectionMonth === 2 && this.state.selectionMonth % 4 === 0 ? 29 : this.days[this.state.selectionMonth];
+    const startofweek = this.calculateDayOfWeek(this.state.selection.getFullYear(), this.state.selection.getMonth() + 1, 1);
+    const days_month = this.state.selection.getMonth() === 1 && this.state.selection.getFullYear() % 4 === 0 ? 29 : this.days[this.state.selection.getMonth()];
     var cur_day = 1;
     var total_weeks = Math.round((days_month + startofweek) / 7 + 0.5);
-    for (var i = 0; i < total_weeks; i++) {
+    for (var i = 1; i <= total_weeks; i++) {
       const to = cur_day + 6 + (cur_day === 1 ? -startofweek : 0);
       weeks.push(
-        <div className="sitcontrol_week">
+        <div key={i} className="sitcontrol_week">
           {cur_day === 1 && this.renderEmpty(startofweek)}
           {this.renderDays(cur_day, Math.min(to, days_month))}
+          {i === total_weeks && this.renderEmpty(i * 7 - days_month - startofweek)}
         </div>
       );
       cur_day = to + 1;
@@ -215,7 +119,7 @@ export default class DatePicker extends React.Component {
     const array = [];
     for (var i = from; i <= to; i++)
       array.push(
-        <div key={i} className="sitcontrol_day">
+        <div key={i} className={cx("sitcontrol_day", this.state.selection.getDate() === i && "picked")}>
           {i}
         </div>
       );
@@ -248,15 +152,16 @@ export default class DatePicker extends React.Component {
   }
 }
 
-DatePicker.defaultProps = {
+Calendar.defaultProps = {
   style: {},
+  defaultValue: new Date(),
   onChange: () => {},
   lang: (navigator.language || navigator.userLanguage || "en").substring(0, 2),
-  format: "yyyy-MM-dd"
+  format: "yyyy-mm-dd"
 };
 
-DatePicker.propTypes = {
-  value: PropTypes.string,
+Calendar.propTypes = {
+  value: PropTypes.any,
   onChange: PropTypes.func,
   format: PropTypes.string
 };
---
+++ src/components/util.js
@@ -0,0 +1,41 @@
+export function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+export function addMonths(date, months) {
+  var result = new Date(date);
+  result.setMonth(result.getMonth() + months);
+  return result;
+}
+export function addYears(date, years) {
+  var result = new Date(date);
+  result.setFullYear(result.getFullYear() + years);
+  return result;
+}
+export function getDate(date, format) {
+  if (typeof date === "string") {
+    return dateFromString(date, format);
+  }
+  return date;
+}
+export function dateFromString(datestr, format) {
+  var normalized = datestr.replace(/[^a-zA-Z0-9]/g, "-");
+  var normalizedFormat = format.toLowerCase().replace(/[^a-zA-Z0-9]/g, "-");
+  var formatItems = normalizedFormat.split("-");
+  var dateItems = normalized.split("-");
+  var monthIndex = formatItems.indexOf("mm");
+  var dayIndex = formatItems.indexOf("dd");
+  var yearIndex = formatItems.indexOf("yyyy");
+  var hourIndex = formatItems.indexOf("hh");
+  var minutesIndex = formatItems.indexOf("ii");
+  var secondsIndex = formatItems.indexOf("ss");
+  var today = new Date();
+  var year = yearIndex > -1 ? dateItems[yearIndex] : today.getFullYear();
+  var month = monthIndex > -1 ? dateItems[monthIndex] - 1 : today.getMonth() - 1;
+  var day = dayIndex > -1 ? dateItems[dayIndex] : today.getDate();
+  var hour = hourIndex > -1 ? dateItems[hourIndex] : today.getHours();
+  var minute = minutesIndex > -1 ? dateItems[minutesIndex] : today.getMinutes();
+  var second = secondsIndex > -1 ? dateItems[secondsIndex] : today.getSeconds();
+  return new Date(year, month, day, hour, minute, second);
+}
