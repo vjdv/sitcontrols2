c58d7245cda8aa9a51f593cb4f1abbbef634e6db
Jassiel <vdiaz@elektra.com.mx>
2019-02-01 12:41:06 -0600

    deprecated

--- src/components/scselect.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import CSelect from "./cselect";
-import s from "./../css/sitcontrol.scss";
-
-export default function Select(props) {
-  return <CSelect ss={s} {...props} />;
-}
